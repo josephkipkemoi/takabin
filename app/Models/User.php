91b327e09f177674afcf6c3c818aadaea864170e
josephkipkemoi <jkemboe@gmail.com>
2023-01-03 14:48:33 +0300

    API Endpoints

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Broadcast;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
@@ -86,4 +87,5 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->hasOne(Address::class);
     }
+
 }
